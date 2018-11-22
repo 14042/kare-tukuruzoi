64c10eae2e0726a4d38720ba91d1c8b4de243a06
klaray <ryousuke.0501.tr@gmail.com>
2018-11-22 11:31:40 +0900

    バス追加

--- kare-/Popuko.java
+++ kare-/Popuko.java
@@ -15,5 +15,9 @@ public class Popuko extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(3);
+}
     }    
 }
